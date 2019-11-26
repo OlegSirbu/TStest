95dd0e853886dd331d19b1972f65bb49e31dccec
Oleg <olegsirbu2@gmail.com>
2019-11-26 21:16:37 +0200

    change fib

--- fib.js
+++ fib.js
@@ -1,17 +1,6 @@
 
 // xn = xn-1 + xn-2
 
-// recurcies
-var fib = (n) => {
-    if(n==1 || n==2) {
-        return 1;
-    }else {
-        return fib(n - 1) + fib(n - 2);
-    }
-};
-console.log(fib(7));
-
-
 // while 
 function fibWhile(num){
     var a = 0, b = 1, temp;
